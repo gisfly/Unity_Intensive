e251267b03d499bec428acdf4fc58d8adee1f959
gisfly <gis.fl@yandex.ru>
2020-01-08 20:38:00 +0700

    Дописаны материалы по ООП

---
+++ OOP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Users anonymous = new Users();
+            Console.WriteLine($"name = {anonymous.name}\nemail = {anonymous.email}\nage = {anonymous.age}\npassword = {anonymous.password}\n");
+            
+            Users Ilya = new Users("admin@mail.ru", "test123");
+            anonymous.SetAll("anon", "admin@mail.ru", 25, "test");
+
+            Ilya.PrintAll();
+            anonymous.PrintAll();
+
+            Users alex = new Users();
+            alex.name = "Алексей";
+            alex.email = "alex@mail.ru";
+            alex.age = 45;
+            alex.SetRole("Модератор");
+
+            Users bob = new Users();
+            bob.name = "Боб";
+            bob.email = "bob@mail.ru";
+            bob.age = 20;
+            bob.SetRole("Редактор");
+
+            Users admin = new Users();
+            admin.name = "Боб";
+            admin.email = "admin@mail.ru";
+            admin.age = 30;
+            admin.administrator.role = "Админ";
+            admin.PrintAll();
+            bob.PrintAll();
+            Console.WriteLine(alex.administrator.role);
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ OOP/Users.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Users
+    {
+        public string name;
+        public string email;
+        public int age;
+        public string password;
+        public Admin administrator = new Admin();
+
+        public Users()
+        {
+            name = "none";
+            email = "info@mail.ru";
+            age = 0;
+            password = "pass";
+        }
+
+        public Users(string _email, string _pass)
+        {
+            email = _email;
+            password = _pass;
+        }
+
+        public void SetAll(string _name, string _email, int _age, string _pass)
+        {
+            name = _name;
+            email = _email;
+            age = _age;
+            password = _pass;
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine($"name = {name} email = {email} age = {age} password = {password} role = {administrator.role}");
+        }
+
+        public void SetRole(string _role)
+        {
+            administrator.role = _role;
+        }
+    }
+}
