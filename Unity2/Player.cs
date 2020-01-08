4612dae0664f19ee1d63a4a24921bf4a3400a729
gisfly <gis.fl@yandex.ru>
2020-01-08 20:39:10 +0700

    Задание 2

---
+++ Unity2/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Unity2
+{
+    class Player
+    {
+        private string name;
+        private VARIANTS weapon;
+
+        public Player()
+        {
+            var rand = new Random();
+            this.weapon = (VARIANTS)rand.Next(0, 3);
+            this.name = "Bot";
+            Console.WriteLine($"{this.name} - {this.weapon}");
+        }
+
+        public Player(VARIANTS weapon, string name)
+        {
+            this.weapon = weapon;
+            this.name = name;
+            Console.WriteLine($"{this.name} - {this.weapon}");
+        }
+
+        public string WhoWins(Player a, Player b)
+        {
+            string result;
+            //PAPER vs ROCK
+            if (a.weapon == VARIANTS.PAPER && b.weapon == VARIANTS.ROCK) result = $"Победил игрок с именем: {a.name}";
+            else if (b.weapon == VARIANTS.PAPER && a.weapon == VARIANTS.ROCK) result = $"Победил игрок с именем: {b.name}";
+            //PAPER vs SCISSORS
+            else if (a.weapon == VARIANTS.PAPER && b.weapon == VARIANTS.SCISSORS) result = $"Победил игрок с именем: {b.name}";
+            else if (b.weapon == VARIANTS.PAPER && a.weapon == VARIANTS.SCISSORS) result = $"Победил игрок с именем: {a.name}";
+            //ROCK vs SCISSORS
+            else if (a.weapon == VARIANTS.ROCK && b.weapon == VARIANTS.SCISSORS) result = $"Победил игрок с именем: {a.name}";
+            else if (b.weapon == VARIANTS.ROCK && a.weapon == VARIANTS.SCISSORS) result = $"Победил игрок с именем: {b.name}";
+            else result = "Ничья!";
+            return result;
+        }
+    }
+
+    enum VARIANTS
+    {
+        PAPER, ROCK, SCISSORS
+    }
+}
---
+++ Unity2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unity2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание к уроку 2. Rock, Paper, Scissors");
+            Console.WriteLine("Необходимо создать класс Player, в котором должны быть реализованы следующие моменты:");
+            Console.WriteLine("1)перечисление для установки выбора: enum VARIANTS;");
+            Console.WriteLine("2)переменные для установки имени, а также определенного варианта из перечисления;");
+            Console.WriteLine("3)два конструктора.Первый ничего не принимает и устанавливает случайное значение из перечисления, а также имя «Bot». Второй конструктор принимает определенный вариант из перечисления и имя для объекта;");
+            Console.WriteLine("4)функция whoWins, которая принимает два объекта и возвращает либо строку «Ничья», либо информацию про игрока, который победил.");
+            Console.WriteLine("\nИгра: Камень,ножницы, бумага!");                            
+            Player bot = new Player();
+            Player alex = new Player(VARIANTS.PAPER, "Alex");
+
+            Console.Write(bot.WhoWins(bot, alex));
+
+            Console.ReadKey();
+        }
+    }
+}
